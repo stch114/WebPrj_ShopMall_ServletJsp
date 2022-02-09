57f4dda9862a91c8cb8c8faf9c19765b36763182
stch114 <stch114@gmail.com>
2022-02-09 18:32:38 +0900

    [UPDATE] 회원가입 시의 데이터 파싱 및 db 작동할 메서드 추가.

--- ShopMall_ServletJsp/src/com/controller/member/AddMemberServlet.java
+++ ShopMall_ServletJsp/src/com/controller/member/AddMemberServlet.java
@@ -6,6 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.dto.MemberDTO;
+import com.service.MemberService;
 
 @WebServlet("/AddMemberServlet")
 public class AddMemberServlet extends HttpServlet {
@@ -17,6 +21,32 @@ public class AddMemberServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		String userid = request.getParameter("userid");
+		String passwd = request.getParameter("passwd");
+		String username = request.getParameter("username");
+		String post = request.getParameter("post");
+		String addr1 = request.getParameter("addr1");
+		String addr2 = request.getParameter("addr2");
+		String phone1 = request.getParameter("phone1");
+		String phone2 = request.getParameter("phone2");
+		String phone3 = request.getParameter("phone3");
+		String email1 = request.getParameter("email1");
+		String email2 = request.getParameter("email2");
+
+		MemberDTO dto = new MemberDTO(userid, passwd, username, post, addr1, addr2, phone1, phone2, phone3, email1,
+				email2);
+
+		MemberService service = new MemberService();
+		int n = service.addMember(dto);
+
+		// redirect로 화면처리 위임하니 session scope로 데이터 저장.
+		HttpSession session = request.getSession();
+		if (n > 0) { // 회원가입이 정상적으로 될 경우(db에 레코드가 저장되었을 때)
+			// main.jsp에서 받아 사용할 데이터.
+			session.setAttribute("addMember", "회원가입 성공");
+		}
+		session.setMaxInactiveInterval(60 * 30); // session 만료시간 30분.
 		response.sendRedirect("main.jsp");
 	}
 
